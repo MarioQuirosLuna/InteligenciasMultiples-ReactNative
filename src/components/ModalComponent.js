c70bf42eab3aa909b3fa49e3ed6c3ed4c4f3c7dc
IsaPortuguez <isabel210399@gmail.com>
2023-06-03 19:47:11 -0600

    Create modal and styles.

--- src/components/ModalComponent.js
+++ src/components/ModalComponent.js
@@ -1,30 +1,77 @@
 import React, { useState } from 'react';
-import { View, Text, Modal, Button } from 'react-native';
+import { StyleSheet } from 'react-native';
+import { View, Text, Modal, Dimensions } from 'react-native';
+import { Button, Image } from 'react-native-elements';
 
-const ModalComponent = ({ text }) => {
-    const [modalVisible, setModalVisible] = useState(false);
+const windowWidth = Dimensions.get('window').width;
+const windowHeigth = Dimensions.get('window').height;
+
+const ModalComponent = ({ title, text, modalVisible, setModalVisible }) => {
 
     return (
         <View>
-            <Button
-                title="Abrir Modal"
-                onPress={() => setModalVisible(true)}
-            />
             <Modal
                 visible={modalVisible}
                 animationType="slide"
+                transparent={true}
                 onRequestClose={() => setModalVisible(false)}
             >
-                <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                    <Text>{text}</Text>
-                    <Button
-                        title="Cerrar Modal"
-                        onPress={() => setModalVisible(false)}
-                    />
+                <View style={styles.modalContent}>
+                    <Image source={require('../../assets/images/alert.png')} style={styles.image}/>
+                    <Text style={styles.title}>{title}</Text>
+                    <Text style={styles.description}>{text}</Text>
+                    <View style={styles.buttonContainer}>
+                        <Button title="Cerrar"
+                            buttonStyle={styles.button}
+                            titleStyle={styles.buttonText}
+                            onPress={() => setModalVisible(false)}
+                        />
+                    </View>
                 </View>
             </Modal>
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    modalContent: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'rgba(0, 0, 5, 0.9)',
+
+        height: windowHeigth ,
+        padding: 30,
+    },
+    image: {
+        width: 100,
+        height: 100,
+    },
+    title: {
+        fontSize: 50,
+        fontWeight: 'bold',
+        marginBottom: 10,
+        color: '#0D4F8B',
+    },
+    description: {
+        fontSize: 20,
+        marginVertical: 20,
+        color: '#fff',
+    },
+    buttonContainer: {
+        flexDirection: 'row',
+    },
+    button: {
+        backgroundColor: '#0D4F8B',
+        margin: 20,
+        borderRadius: 50,
+    },
+    buttonText: {
+        color: '#FFFFFF',
+        fontSize: 20,
+        marginHorizontal: 20,
+        fontWeight: 'bold',
+    },
+});
 
 export default ModalComponent;
